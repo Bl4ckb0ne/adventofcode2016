9f3e6a4e7449ea3918f23aaac642b66040e4bbf2
Simon Zeni <simonzeni@gmail.com>
2016-12-04 15:17:26 -0500

    d3p2 rise of the tuple raider

--- day3/main.cpp
+++ day3/main.cpp
@@ -2,28 +2,73 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+#include <vector>
+#include <tuple>
 
 int main(int argc, char* argv[])
 {
-    std::string line;
-    int total = 0;
+    std::vector<std::tuple<int, int, int>> triangles;
 
     do
     {
+        std::string line;
         std::getline(std::cin, line);   
-        std::istringstream iss(line);
-        
-        int a, b, c = 0;
-        iss >> a;
-        iss >> b;
-        iss >> c;
+    
+        if(line.size() == 0)
+            break;
 
-        if((a + b > c) && (a + c > b) && (c + b > a))
-            total++;
- 
+        std::istringstream iss(line);        
+        int a, b, c;
+        iss >> a >> b >> c;
+        triangles.push_back(std::make_tuple(a, b, c));
+    
     } while (std::cin.good());
 
-    std::cout << total << '\n';
+    // part 1
+    int total1 = 0;
+    for(auto &it : triangles)
+    {
+        int a = std::get<0>(it);
+        int b = std::get<1>(it);
+        int c = std::get<2>(it);
+        
+        if((a + b > c) && (c + b > a) && (a + c > b))
+            total1++;
+    }
+    std::cout << "Part 1 : " << total1 << " triangles\n";
+
+    // part 2
+    int total2 = 0;
+    for(size_t i = 0; i < triangles.size();)
+    {
+        int a1 = std::get<0>(triangles[i]);
+        int b1 = std::get<1>(triangles[i]);
+        int c1 = std::get<2>(triangles[i]);
+
+        i++;
+        
+        int a2 = std::get<0>(triangles[i]);
+        int b2 = std::get<1>(triangles[i]);
+        int c2 = std::get<2>(triangles[i]);
+
+        i++;
+        
+        int a3 = std::get<0>(triangles[i]);
+        int b3 = std::get<1>(triangles[i]);
+        int c3 = std::get<2>(triangles[i]);
+        
+        i++;
+               
+        if((a1 + a2 > a3) && (a3 + a1 > a2) && (a2 + a3 > a1))
+            total2++;
+               
+        if((b1 + b2 > b3) && (b3 + b1 > b2) && (b2 + b3 > b1))
+            total2++;
+               
+        if((c1 + c2 > c3) && (c3 + c1 > c2) && (c2 + c3 > c1))
+            total2++;
+    }
+    std::cout << "Part 2 : " << total2 << " triangles\n";
 
     return 0;
 }
